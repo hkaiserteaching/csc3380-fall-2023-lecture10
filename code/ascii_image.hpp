2b34b069a57044650295a7466195dc25168c4bda
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2023-09-23 17:46:20 -0500

    Minor changes

--- code/ascii_image.hpp
+++ code/ascii_image.hpp
@@ -36,9 +36,9 @@ struct image
     {
     }
 
-    // construct an 'empty' image of the given size
-    image(int width, int height)
-      : data(height, std::string(width, ' '))
+    // construct an optionally 'space-filled' image of the given size
+    image(int width, int height, char fill = ' ')
+      : data(height, std::string(width, fill))
     {
     }
 
@@ -54,12 +54,6 @@ struct image
     std::string::size_type height() const
     {
         return data.size();
-    }
-
-    // append a single line to the bottom of the image
-    void append(std::string const& line)
-    {
-        data.push_back(line);
     }
 
     // expose underlying iterators
@@ -100,6 +94,19 @@ struct image
     std::string const& operator[](std::size_t idx) const
     {
         return data[idx];
+    }
+
+    // append a single line to the bottom of the image
+    void append(std::string const& line)
+    {
+        data.push_back(line);
+    }
+
+    // append another image
+    void append(image const& img)
+    {
+        for (auto const& s : img)
+            data.push_back(s);
     }
 
     // print the 'image'
